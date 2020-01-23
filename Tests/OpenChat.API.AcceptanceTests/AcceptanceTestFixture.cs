b2a924ee145f9693727dac3f551a73b9bd1be452
Dragoș Rogojan <dragos.rogojan@gmail.com>
2020-01-24 01:05:52 +0200

    Set up xunit logging and Acceptance Test Fixture

---
+++ Tests/OpenChat.API.AcceptanceTests/AcceptanceTestFixture.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace OpenChat.API.AcceptanceTests
+{
+    public class AcceptanceTestFixture : WebApplicationFactory<OpenChat.API.Startup>
+    {
+        public ITestOutputHelper TestOutputHelper { get; set; }
+
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            var hostBuilder = base.CreateHostBuilder();
+            hostBuilder.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddXUnit(TestOutputHelper);
+            });
+
+            return hostBuilder;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.json");
+
+            builder.ConfigureAppConfiguration(configurationBuilder => { configurationBuilder.AddJsonFile(configPath); });
+
+            builder.ConfigureServices(services =>
+            {
+                var buildServiceProvider = services.BuildServiceProvider();
+
+                using (var scope = buildServiceProvider.CreateScope())
+                {
+                    // var scopeServiceProvider = scope.ServiceProvider;
+                    // var myService = scopeServiceProvider.GetRequiredService<MyService>();
+                }
+            });
+        }
+    }
+}
