4ff063bd7e28a12de9a52062c6df55665aebca38
Dragos Rogojan <dragos.rogojan@gmail.com>
2020-02-05 17:39:22 +0200

    Started PostsByUser Acceptance test

--- Tests/OpenChat.API.AcceptanceTests/PostApiTests.cs
+++ Tests/OpenChat.API.AcceptanceTests/PostApiTests.cs
@@ -34,6 +34,7 @@ namespace OpenChat.API.AcceptanceTests
             };
 
             var registeredUserResponse = await client.PostAsJsonAsync("api/users", user);
+            registeredUserResponse.StatusCode.Should().Be(HttpStatusCode.Created);
 
             var registeredUser = await registeredUserResponse.Content.ReadAsJsonAsync<JObject>();
             var userId = registeredUser.Value<string>("id");
@@ -53,6 +54,59 @@ namespace OpenChat.API.AcceptanceTests
             createdPost.Value<string>("text").Should().Be(post.text);
             // createdPost.Value<DateTime>("dateTime") should have specific format
             // TODO - add CORS and other needed feature to the branch for the workshop
+        }
+
+        [Fact]
+        public async Task Return_the_posts_for_a_user_in_reverse_chronological_order()
+        {
+            var user = new
+            {
+                username = "John",
+                password = "john123",
+                about = "I like to say hello"
+            };
+
+            var registeredUserResponse = await client.PostAsJsonAsync("api/users", user);
+            registeredUserResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var registeredUser = await registeredUserResponse.Content.ReadAsJsonAsync<JObject>();
+            var userId = registeredUser.Value<string>("id");
+
+            var firstPost = new
+            {
+                text = "Hello World! This is John."
+            };
+
+            var secondPost = new {
+                text = "Hey there! It's me again."
+            };
+
+            var createfirstPostReponse = await client.PostAsJsonAsync($"api/users/{userId}/timeline", firstPost);
+            createfirstPostReponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var firstCreatedPost = await createfirstPostReponse.Content.ReadAsJsonAsync<JObject>();
+            Guid.Parse(firstCreatedPost.Value<string>("postId")).Should().NotBeEmpty();
+            firstCreatedPost.Value<string>("userId").Should().Be(userId);
+            firstCreatedPost.Value<string>("text").Should().Be(firstPost.text);
+            // firstCreatedPost.Value<DateTime>("dateTime") should have specific format
+
+            var secondPostReponseResponse = await client.PostAsJsonAsync($"api/users/{userId}/timeline", secondPost);
+            secondPostReponseResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var secondCreatedPost = await createfirstPostReponse.Content.ReadAsJsonAsync<JObject>();
+            Guid.Parse(secondCreatedPost.Value<string>("postId")).Should().NotBeEmpty();
+            secondCreatedPost.Value<string>("userId").Should().Be(userId);
+            secondCreatedPost.Value<string>("text").Should().Be(secondPost.text);
+            // secondCreatedPost.Value<DateTime>("dateTime") should have specific format
+
+            var postsByUserResponse = await client.GetAsync($"api/users/{userId}/timeline");
+            postsByUserResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var postsByUser = await postsByUserResponse.Content.ReadAsJsonAsync<JObject[]>();
+            postsByUser.Length.Should().Be(2);
+
+            postsByUser[0].Value<string>("text").Should().Be(secondPost.text);
+            postsByUser[1].Value<string>("text").Should().Be(firstPost.text);
         }
     }
 }
